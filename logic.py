2005315d30a05c5f35e73a713c0f177acae9611f
Abdeali Bootwala <132557392+abdealib520@users.noreply.github.com>
2023-10-30 07:40:30 +0530

    Update logic.py

--- logic.py
+++ logic.py
@@ -6,7 +6,7 @@ from nltk.corpus import stopwords
 import nltk
 nltk.download('stopwords')
 nltk.download('punkt')
-nltk.download('omw-1.4.1')
+nltk.download('omw-1.4')
 stop_words = set(stopwords.words('english'))
 from nltk.stem import WordNetLemmatizer
 wordnet_lemmatizer = WordNetLemmatizer()
